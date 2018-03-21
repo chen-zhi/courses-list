37dff962345eb3acaa3402aa547ed94e1998f6bd
Zhi <chenzhi.finland@gmail.com>
2018-03-21 17:54:16 +0200

    change to function components

--- src/components/CourseList/CourseList.js
+++ src/components/CourseList/CourseList.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import "./CourseList.css";
 
-class CourseList extends Component {
-  render() {
-    const courses = this.props.courses.map(course => (
-      <div className="courseItem" key={course.id}>
-        <h3>{course.title}</h3>
-        <div>{course.feed}</div>
-      </div>
-    ));
-    return <div className="CourseList">{courses}</div>;
-  }
+function CourseList(props) {
+  const courses = props.courses.map(course => (
+    <div className="courseItem" key={course.id}>
+      <h3>{course.title}</h3>
+      <div>{course.feed}</div>
+    </div>
+  ));
+  return <div className="CourseList">{courses}</div>;
 }
 
 export default CourseList;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="Header">
-        <h1>Course List</h1>
-        <input placeholder="Search" onChange={(event) => this.props.coursesSearch(event.target.value)}/>
-      </div>
-    );
-  }
+function Header(props) {
+  return (
+    <div className="Header">
+      <h1>Course List</h1>
+      <input
+        placeholder="Search"
+        onChange={event => props.coursesSearch(event.target.value)}
+      />
+    </div>
+  );
 }
 
 export default Header;
